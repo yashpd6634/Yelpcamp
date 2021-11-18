402768531ccb854aef634f153781a2531db69c7d
Yash Dewangan <yashpd13@gmail.com>
2021-11-18 18:52:06 +0530

    updates

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -71,6 +71,19 @@ map.on("load", () => {
     },
   });
 
+
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+    positionOptions: {
+    enableHighAccuracy: true
+    },
+    // When active the map will receive updates to the device's location as it changes.
+    trackUserLocation: true,
+    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+    showUserHeading: true
+    })
+    );
+
   // inspect a cluster on click
   map.on("click", "clusters", (e) => {
     const features = map.queryRenderedFeatures(e.point, {
