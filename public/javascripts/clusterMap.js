e404f741c320903ec594ae2c0ac3309397c8dace
Yash Dewangan <yashpd13@gmail.com>
2021-11-18 18:26:03 +0530

    updates

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/dark-v10",
-  center: [-103.5917, 40.6699],
-  zoom: 3,
+  center: [79.61187538771085, 23.11434254065905], 
+  zoom: 5,
 });
 
 map.addControl(new mapboxgl.NavigationControl());
